decb78560e8df46b376c9b788e1b67017fce3a65
imikkyhh <rickymahardika33@smk.belajar.id>
2021-02-25 18:40:35 +0700

    Add files via upload

---
+++ PROProtocol/GameServer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net;
+
+namespace PROProtocol
+{
+    public enum GameServer
+    {
+        Silver,
+        Gold
+    }
+
+    public static class GameServerExtensions
+    {
+        public static IPEndPoint GetAddress(this GameServer server)
+        {
+            switch (server)
+            {
+                case GameServer.Silver:
+                    return new IPEndPoint(IPAddress.Parse("185.70.107.113"), 800);
+                case GameServer.Gold:
+                    return new IPEndPoint(IPAddress.Parse("185.70.107.113"), 801);
+            }
+            return null;
+        }
+
+        public static GameServer FromName(string name)
+        {
+            switch (name.ToUpperInvariant())
+            {
+                case "SILVER":
+                    return GameServer.Silver;
+                case "GOLD":
+                    return GameServer.Gold;
+            }
+            throw new Exception("The server " + name + " does not exist");
+        }
+    }
+}
---
+++ PROShine/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+
+namespace PROShine
+{
+    public partial class App : Application
+    {
+        public static string Name { get; private set; }
+        public static string Version { get; private set; }
+        public static string Author { get; private set; }
+        public static string Description { get; private set; }
+        public static bool IsBeta { get; private set; }
+
+        public static void InitializeVersion()
+        {
+            Assembly assembly = typeof(App).Assembly;
+            AssemblyName assemblyName = assembly.GetName();
+            Name = assemblyName.Name;
+            Version = assemblyName.Version.ToString(3) + "-beta2";
+            IsBeta = true;
+            Author = ((AssemblyCompanyAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCompanyAttribute), false)).Company;
+            Description = ((AssemblyDescriptionAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyDescriptionAttribute), false)).Description;
+        }
+    }
+}
